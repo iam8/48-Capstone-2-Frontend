e37cd57b4debe58ae9d19f203e98153659638d8f
alexm <alex-m@outlook.com>
2023-12-09 11:49:24 -0800

    Add 2 tests for request()

--- colors/src/api.test.js
+++ colors/src/api.test.js
@@ -3,9 +3,46 @@ import ColorsApi from "./api";
 
 jest.mock("axios");
 
+console.log("API TESTS -------------------------------------------------------------------------");
+
 
 test("This is a TEST test", () => {
     expect(1).toBeTruthy();
 })
 
 
+describe("request()", () => {
+    ColorsApi.token = "testtoken";
+
+    test("Should return data fetched by axios", async () => {
+        const resp = {data: "test data"};
+        axios.mockImplementation(() => Promise.resolve(resp));
+
+        const result = await ColorsApi.request("testendpoint");
+        expect(result).toBe("test data");
+    })
+
+    test("Throws exception with array of error messages on request failure", async () => {
+        expect.assertions(2);
+
+        const msg1 = "Msg 1";
+        const msg2 = ["Msg 1", "Msg 2"];
+
+        const resp1 = {response: {data: {error: {message: msg1}}}};
+        const resp2 = {response: {data: {error: {message: msg2}}}};
+
+        axios.mockImplementationOnce(() => Promise.reject(resp1));
+        axios.mockImplementationOnce(() => Promise.reject(resp2));
+
+        const expected = [[msg1], msg2];
+
+        for (let i = 0; i < 2; i++) {
+            try {
+                await ColorsApi.request("testendpoint");
+            } catch(err) {
+                expect(err).toEqual(expected[i]);
+            }
+        }
+    })
+
+})
