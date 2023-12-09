d71a69884c2239c2299fa9be71b1f551bdcfb230
alexm <alex-m@outlook.com>
2023-12-08 16:39:22 -0800

    Remove reminder (shouldn't be here anyway)

--- colors/src/App.js
+++ colors/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
     return (
@@ -7,7 +7,6 @@ function App() {
             <header className="App-header">
                 Colors App.
                 Whee, look at all the colors here that don't exist yet!
-                Reminder: find a better app icon to use.
             </header>
         </div>
     );
