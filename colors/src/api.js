c8d080a4e5d628323eaf0bb58197fa369a54ff60
alexm <alex-m@outlook.com>
2023-12-08 17:03:28 -0800

    Initialize Colors api class and write request method for building and sending Axios requests

--- colors/src/api.js
+++ colors/src/api.js
@@ -1,3 +1,35 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+
+/**
+ * Colors API class.
+ *
+ * Static class containing methods used to get/send to the Colors project backend API.
+ */
+class ColorsApi {
+    static token;
+
+    /** Send request to endpoint via Axios with (optional) data and method. */
+    static async request(endpoint, data={}, method="get") {
+
+        // Pass authorization token via request header.
+        const url = `${BASE_URL}/${endpoint}`;
+        const headers = {Authorization: `Bearer ${ColorsApi.token}`};
+        const params = (method === "get") ? data : {};
+
+        try {
+            return (await axios({url, method, data, headers, params})).data;
+        } catch(err) {
+            console.error("API Error:", err.response);
+            let message = err.response.data.error.message;
+            throw Array.isArray(message) ? message : [message];
+        }
+    }
+
+    // Individual API routes ----------------------------------------------------------------------
+}
+
+
+export default ColorsApi;
