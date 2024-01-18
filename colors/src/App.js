4a9559eaef0d81f7754105eeac2d324d931c7597
alexm <alex-m@outlook.com>
2024-01-17 17:15:05 -0800

    Add imports and temporary page content

--- colors/src/App.js
+++ colors/src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
 import './App.css';
 
 
@@ -7,6 +10,13 @@ function App() {
             <header className="App-header">
                 Colors App.
                 Whee, look at all the colors here that don't exist yet!
+
+                Here are some react-router-dom links (temporary) to the auth pages. The links do
+                not work yet.
+                {/* <Link to="/login">Token</Link> */}
+                {/* <Link to="/signup">Sign up</Link> */}
+                <a href="/login">Token/login</a>
+                <a href="/signup">Sign up</a>
             </header>
         </div>
     );
