81c727c466dac64cca7ebeb4607df31b1f0bfb11
alexm <alex-m@outlook.com>
2024-01-18 17:15:29 -0800

    Add docstring for LoginForm

--- colors/src/auth/LoginForm.js
+++ colors/src/auth/LoginForm.js
@@ -3,6 +3,17 @@ import { useHistory } from "react-router-dom";
 import { Alert, Button, Card, CardBody, Form, FormGroup, Label, Input } from "reactstrap";
 
 
+/**
+ * Route: /login
+ *
+ * Form used to log in to Colors site.
+ *
+ * Manages state updates on changes to the form inputs.
+ *
+ * On successful login, calls login (function prop) and redirects to homepage.
+ *
+ * On login failure, displays alert and error message.
+ */
 function LoginForm({login}) {
 
     return (
