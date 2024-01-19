63deb899f6f0e886fe3d276116f59f40e89e25b0
alexm <alex-m@outlook.com>
2024-01-18 17:26:35 -0800

    Set up component state; add logic to handle changes to form inputs

--- colors/src/auth/LoginForm.js
+++ colors/src/auth/LoginForm.js
@@ -15,6 +15,28 @@ import { Alert, Button, Card, CardBody, Form, FormGroup, Label, Input } from "re
  * On login failure, displays alert and error message.
  */
 function LoginForm({login}) {
+    const INIT_FORM = {
+        username: "",
+        password: ""
+    };
+
+    const [formData, setFormData] = useState(INIT_FORM);
+    const [formErrors, setFormErrors] = useState([]);
+    const history = useHistory();
+
+    /** Handle changes to inputs. */
+    const handleChange = (evt) => {
+        const [name, value] = evt.target;
+        setFormErrors([]);
+        setFormData((formData) => ({
+            ...formData,
+            [name]: value
+        }))
+    }
+
+    /** Form submission - log in user and redirect to home on success. */
+
+    /** Show an alert message on failure to log in. */
 
     return (
         <div className="LoginForm">
