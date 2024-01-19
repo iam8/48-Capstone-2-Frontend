1ff16aca0b754ff6a251fa6cec0953dd2f7c03f0
alexm <alex-m@outlook.com>
2024-01-18 16:53:16 -0800

    Add imports; wrap Routes component in BrowserRouter component

--- colors/src/App.js
+++ colors/src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter} from "react-router-dom";
 
+import ColorsApi from './api';
 import Routes from './routes/Routes';
+import {UserContext} from './auth/UserContext';
 import './App.css';
 
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                Colors App.
-                Whee, look at all the colors here that don't exist yet!
-            </header>
+            <BrowserRouter>
+                <header className="App-header">
+                    Colors App.
+                    Whee, look at all the colors here that don't exist yet!
+                </header>
 
-            <Routes />
+                <Routes />
+            </BrowserRouter>
         </div>
     );
 }
