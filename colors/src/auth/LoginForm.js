82f810d076ead706f60168c566fda99aa1b0b197
alexm <alex-m@outlook.com>
2024-01-18 17:11:33 -0800

    Add initial login form component definition

--- colors/src/auth/LoginForm.js
+++ colors/src/auth/LoginForm.js
@@ -1,3 +1,16 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Alert, Button, Card, CardBody, Form, FormGroup, Label, Input } from "reactstrap";
+
+
+function LoginForm({login}) {
+
+    return (
+        <div className="LoginForm">
+            <h2>Log In</h2>
+        </div>
+    )
+}
+
+
+export default LoginForm;
