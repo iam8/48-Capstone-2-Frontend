0a1d6d2ab2b02d28f5a3cff5b9515e7262b998fb
alexm <alex-m@outlook.com>
2024-01-25 12:41:37 -0800

    Add temp comment with notes

--- colors/src/auth/SignupForm.js
+++ colors/src/auth/SignupForm.js
@@ -1,3 +1,12 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Alert, Button, Card, CardBody, Form, FormGroup, Label, Input } from "reactstrap";
+
+/**
+ * Form should include fields:
+ *
+ * - Username
+ * - Password
+ * - First name
+ * - Last name
+ */
