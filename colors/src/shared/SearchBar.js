d1461a6aa42753030c0eaf7b36109c2fc00ffe3a
alexm <alex-m@outlook.com>
2024-02-13 15:42:04 -0800

    Add initial code, as in Jobly

--- colors/src/shared/SearchBar.js
+++ colors/src/shared/SearchBar.js
@@ -8,7 +8,48 @@ import {Button, Form, Label, Input, Row, Col} from "reactstrap";
  * Calls the searchFor method (passed by parent), which performs the data retrieval.
  */
 function SearchBar({searchFor}) {
+    const [searchValue, setSearchValue] = useState("");
 
+    /** Update search bar field. */
+    const handleChange = (evt) => {
+        setSearchValue(evt.target.value);
+    }
+
+    /** Call searchFor() to redirect and retrieve color data. */
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        searchFor(searchValue || undefined);
+        setSearchValue("");
+    }
+
+    return (
+        <div className="SearchBar">
+            <Form onSubmit={handleSubmit}>
+                <Row>
+                    <Col>
+                        <Label
+                            className="visually-hidden"
+                            htmlFor="search-input"
+                        >
+                            Search for a color by hex value
+                        </Label>
+
+                        <Input
+                            id="search-input"
+                            name="searchValue"
+                            placeholder="Enter a 6-digit hex value"
+                            value={searchValue}
+                            onChange={handleChange}
+                            bsSize="lg"
+                        />
+                    </Col>
+                    <Col>
+                        <Button type="submit" color="primary" size="lg">Retrieve data</Button>
+                    </Col>
+                </Row>
+            </Form>
+        </div>
+    )
 }
 
 
