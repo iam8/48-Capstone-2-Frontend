5a06fabb8094d4a53f2757307996ad7be3366eb3
alexm <alex-m@outlook.com>
2024-02-13 15:13:39 -0800

    Set up file for SearchBar component - imports, docstring, function def

---
+++ colors/src/shared/SearchBar.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import {Button, Form, Label, Input, Row, Col} from "reactstrap";
+
+
+/**
+ * Search bar. Used on /colors and /schemes pages to retrieve color and scheme data.
+ *
+ * Calls the searchFor method (passed by parent), which performs the data retrieval.
+ */
+function SearchBar({searchFor}) {
+
+}
+
+
+export default SearchBar;
