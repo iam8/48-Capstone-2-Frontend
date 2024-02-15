d10c92033c6b8be3a183a492247ea3b82bd09e70
alexm <alex-m@outlook.com>
2024-02-15 15:16:57 -0800

    Add more detail to docstring

--- colors/src/colors/ColorDetails.js
+++ colors/src/colors/ColorDetails.js
@@ -4,7 +4,8 @@ import React, {useState, useEffect} from "react";
 /**
  * Display details about a specific color, designated by its 6-digit hex value.
  *
- * Calls external API at thecolorapi.com to get detailed info about a color
+ * Calls external API at thecolorapi.com to get detailed info about a color, then displays
+ * selected data from the result.
  */
 function ColorDetails({hex}) {
     return (
