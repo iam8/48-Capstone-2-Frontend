18b40498ad4ee4c5dc8ef8fd258d061ffec73ae9
alexm <alex-m@outlook.com>
2024-02-15 15:31:17 -0800

    Add prop and state details to docstring; set up state and useEffect callback

--- colors/src/colors/ColorDetails.js
+++ colors/src/colors/ColorDetails.js
@@ -6,8 +6,22 @@ import React, {useState, useEffect} from "react";
  *
  * Calls external API at thecolorapi.com to get detailed info about a color, then displays
  * selected data from the result.
+ *
+ * Props
+ *  - hex (string): hex value for a color to retrieve data for
+ * State
+ *  - colorData (object): data fetched from external API for current color
  */
 function ColorDetails({hex}) {
+    const [colorData, setColorData] = useState(null);
+
+    useEffect(() => {
+        async function fetchColorData() {
+
+        }
+    })
+
+
     return (
         <div className="ColorDetails">
             This is in the ColorDetails component.
