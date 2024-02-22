fa850bbc69ae47bf48a0279fc0328281dc54e396
alexm <alex-m@outlook.com>
2024-02-21 17:28:17 -0800

    Create context for collections data

---
+++ colors/src/collections/CollectionsContext.js
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+
+/** React context to hold collections data, which will be viewed and/or accessed throughout the
+ * app.
+ */
+const CollectionsContext = createContext(null);
+
+export default CollectionsContext;
